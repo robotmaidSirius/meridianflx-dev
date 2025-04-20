e46e949e1b0b48e62d18fc0c5fad08e97a87d19f
TenrouHoshina <masiro.to.akari@gmail.com>
2025-04-20 16:38:12 +0900

    Meridim90.hppの追加

---
+++ lib/Meridian/Core/MeridianCore/include/Meridim90.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file Meridim90.hpp
+ * @brief Meridianで定義されているMeridim90型
+ * @version 1.0.0
+ * @date 2025-04-20
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#ifndef __MERIDIAN_CORE_MERIDIM_MERIDIM90_HPP__
+#define __MERIDIAN_CORE_MERIDIM_MERIDIM90_HPP__
+
+#include <stdint.h>
+
+namespace meridian {
+namespace core {
+namespace meridim {
+
+} // namespace meridim
+} // namespace core
+} // namespace meridian
+
+#endif // __MERIDIAN_CORE_MERIDIM_MERIDIM90_HPP__
